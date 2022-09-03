b4e90462b1ffb6ae9653da2e4d00184ced0a9fb9
busealkan <buse_alkan10@hotmail.com>
2022-09-03 14:39:27 +0300

    gif commit

--- app/src/main/java/com/busealkan/buse_alkan_odev2/util/Constants.kt
+++ app/src/main/java/com/busealkan/buse_alkan_odev2/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.busealkan.buse_alkan_odev2.util
 
 object Constants {
-     val USERNAME = "busealkan"
+     val USERNAME = "buse"
      val PASSWORD = "1234"
      val EMAIL = "buse_alkan10@hotmail.com"
 }
